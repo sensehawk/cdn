fb1ab714e109e72f7de468b1d4ab35acd020d8cb
karthikeyanVue <45160801+karthikeyanVue@users.noreply.github.com>
2024-03-09 14:04:21 +0400

    feat: add latest pdftron files

---
+++ pdftronv2/AzurePartRetriever.chunk.js
@@ -0,0 +1,3 @@
+/** Notice * This file contains works from many authors under various (but compatible) licenses. Please see core.txt for more information. **/
+(function(){(window.wpCoreControlsBundle=window.wpCoreControlsBundle||[]).push([[3],{562:function(xa,ta,n){n.r(ta);var qa=n(0),na=n(187);xa=n(556);n=n(477);xa=function(oa){function ha(la,ia,z,aa){return oa.call(this,la,ia,z,aa)||this}Object(qa.c)(ha,oa);ha.prototype.m3=function(){return{start:this.i2-na.a,stop:this.i2}};ha.prototype.ix=function(la){var ia=this;this.mr(this.url,{start:0,stop:1},function(z,aa,r){if(z)return la(z);z=r.request.getResponseHeader("Content-Range");ia.i2=z.split("/")[1];oa.prototype.ix.call(ia,
+la)})};return ha}(xa["default"]);Object(n.a)(xa);Object(n.b)(xa);ta["default"]=xa}}]);}).call(this || window)
